d69d4ba523fb2c0785cd9960b505bde661a52cc5
zzzzzpaul <zzzzzpaul@qq.com>
2021-01-28 13:30:06 +0800

    upload a login shell script

--- dn_Jenkins_postbuild_01.sh
+++ dn_Jenkins_postbuild_01.sh
@@ -34,6 +34,7 @@ echo "   '----'                  '----'                "
 echo "脚本作者:度牛电商软件开发部--唐保罗"
 echo '没有完美的脚本, 如果安装过程报错, 请查找相关资料修改脚本, 或联系脚本作者获得支持'
 # 进入到target目录
+# 经测试脚本拉取成功但解析执行失败, 错误定位在43行else, 但经检查未发现错误
 STAT=2
 TARGETPATH="/var/lib/jenkins/workspace/dn_gate_dev/target"
 cd $TARGETPATH
@@ -45,7 +46,7 @@ else
 	$STAT=1
 fi 
 if [ $STAT==0 ] then;
-	docker build -f $TARGETPATH/Dockerfile mcmsimg
+	sudo docker build -f $TARGETPATH/Dockerfile mcmsimg
 	$STAT=0
 else 
 	echo "制作镜像出错,请检查"
@@ -53,9 +54,9 @@ else
 fi 
 # 将镜像推送到harbor仓库
 # tag镜像
-docker login -u tpaul -p Tpaul123 192.168.0.45
+sudo docker login -u tpaul -p Tpaul123 192.168.0.45
 if [ $STAT==0 ] then;
-	docker tag mcmsimg 192.168.0.45/dn_gate_dev/mcmsimg:1.00
+	sudo docker tag mcmsimg 192.168.0.45/dn_gate_dev/mcmsimg:1.00
 	$STAT=0
 else 
 	echo "镜像tag出错,请检查"
@@ -63,11 +64,11 @@ else
 fi 
 # 推送镜像
 if [ $STAT==0 ] then;
-	docker push 192.168.0.45/dn_gate_dev/mcmsimg:1.00
+	sudo docker push 192.168.0.45/dn_gate_dev/mcmsimg:1.00
 	$STAT=0
 else 
 	echo "推送镜像出错,请检查"
 	$STAT=1
 fi 	
 echo "脚本执行完毕,如有报错请检查脚本,退出Harbor服务器登录" 
-docker logout
+sudo docker logout
