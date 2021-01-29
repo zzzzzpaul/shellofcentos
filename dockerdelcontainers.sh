0ae81906d8971e58bfee7b5a3ec795ec86c2c370
zzzzzpaul <zzzzzpaul@qq.com>
2021-01-29 16:09:55 +0800

    updated ff

--- dockerdelcontainers.sh
+++ dockerdelcontainers.sh
@@ -2,8 +2,7 @@
 RMLIST=$(sudo docker ps -a -q)
 if [ $RMLIST == "" ] then;
 	echo "未找到运行中的docker容器"
-fi
-if [ $RMLIST != "" ] then;
+else
 	echo "停止并删除运行中的容器" &&\
 	sudo docker stop $RMLIST &&\
 	sudo docker rm $RMLIST
