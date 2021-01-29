4d839cb18a5ba15105a7fe5032893bb7db5ef182
zzzzzpaul <zzzzzpaul@qq.com>
2021-01-29 10:08:07 +0800

    update del shell

--- deleteall.sh
+++ deleteall.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 RMLIST=`docker ps -a -q`
 RMILIST=`docker images -a -q`
-sudo su && sudo docker stop $RMLIST && sudo docker rm $RMLIST && sudo docker rmi $RMILIST -f
+sudo docker stop $RMLIST && sudo docker rm $RMLIST && sudo docker rmi $RMILIST -f
