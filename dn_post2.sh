ea18b4b56aa6d47403c01760c7fee212cfda1310
zzzzzpaul <zzzzzpaul@qq.com>
2021-01-30 16:02:51 +0800

    updated post2

--- dn_post2.sh
+++ dn_post2.sh
@@ -17,5 +17,7 @@ wget -O cleari.sh https://raw.githubusercontent.com/zzzzzpaul/shellofcentos/mast
 bash cleari.sh ||\
 sudo docker login -u tpaul -p Tpaul123 192.168.0.45 &&\
 sudo docker pull 192.168.0.45/dn_gate_dev/mcmsimg:1.00 &&\
-sudo docker run -d --name mcms -p 8080:8080 192.168.0.45/dn_gate_dev/mcmsimg:1.00 &&\
-sudo docker logout 
+sudo docker run -d --name mcms -p 8080:8080 --restart=always 192.168.0.45/dn_gate_dev/mcmsimg:1.00 &&\
+sudo docker logout &&\
+echo "重启服务器中, 请耐心等待..." &&\
+init 6
