a88685e1ff0384ba08713ab9c034dc5126b439c9
zzzzzpaul <zzzzzpaul@qq.com>
2021-02-05 14:33:02 +0800

    updated frpsc

--- installfrpc2.sh
+++ installfrpc2.sh
@@ -52,12 +52,12 @@ echo "脚本作者:度牛电商软件开发部--XXX"
 echo '没有完美的脚本, 如果安装过程报错, 请查找相关资料修改脚本, 或联系脚本作者获得支持'
 sleep 3
 clear
-FRPPATH=/root/frp_0.35.1_darwin_amd64
+FRPPATH=/root/frp_0.35.1_linux_amd64
 echo "开始安装frp服务端, 脚本报错请先检查脚本, 并确定已安装docker, docker-compose, 并开放7000端口" &&\
 	echo "下载frp包" &&\
-	wget https://github.com/fatedier/frp/releases/download/v0.35.1/frp_0.35.1_darwin_amd64.tar.gz &&\
+	wget https://github.com/fatedier/frp/releases/download/v0.35.1/frp_0.35.1_linux_amd64.tar.gz &&\
 	echo "解压frp安装包" &&\
-	tar -vxf frp_0.35.1_darwin_amd64.tar.gz &&\
+	tar -vxf frp_0.35.1_linux_amd64.tar.gz &&\
 	cd $FRPPATH &&\
 	cp $FRPPATH/frps.ini $FRPPATH/frps.ini.b &&\
 	cp $FRPPATH/frpc.ini $FRPPATH/frpc.ini.b &&\
@@ -67,13 +67,20 @@ echo "开始安装frp服务端, 脚本报错请先检查脚本, 并确定已安�
 	chmod +x $FRPFILE &&\
 	echo '[common]' > $FRPFILE &&\
 	echo 'server_addr = 8.136.100.199' >> $FRPFILE &&\
-	echo 'server_port = 7000' >> 
+	echo 'server_port = 7000' >> $FRPFILE &&\
 	echo 'token = 7ujm*IK<' >> $FRPFILE &&\
+	echo '' >> $FRPFILE &&\
 	echo '[ssh]' >> $FRPFILE &&\
 	echo 'type = tcp' >> $FRPFILE &&\
 	echo 'local_ip = 127.0.0.1' >> $FRPFILE &&\
 	echo 'local_port = 22' >> $FRPFILE &&\
 	echo 'remote_port = 7001' >> $FRPFILE &&\
+	echo '' >> $FRPFILE &&\
+	echo '[web]' >> $FRPFILE &&\
+	echo 'type = tcp' >> $FRPFILE &&\
+	echo 'local_ip = 127.0.0.1' >> $FRPFILE &&\
+	echo 'local_port = 262' >> $FRPFILE &&\
+	echo 'remote_port = 7002' >> $FRPFILE &&\
 	echo "启动frp服务" &&\
 	$FRPPATH/frps -c $FRPFILE &&\
 	echo "frp服务器搭建成功"
