8cbd730bd8d9f7118af9a03a0b002700175b0575
zzzzzpaul <zzzzzpaul@qq.com>
2021-02-24 09:54:16 +0800

    updated post2

--- dn_post2.sh
+++ dn_post2.sh
@@ -17,6 +17,6 @@ wget -O cleari.sh https://raw.githubusercontent.com/zzzzzpaul/shellofcentos/mast
 bash cleari.sh ||\
 sudo docker login -u tpaul -p Tpaul123 192.168.0.45 &&\
 sudo docker pull 192.168.0.45/dn_gate_dev/mcmsimg:1.00 &&\
-sudo docker run -d --name mcms -p 8080:8080 -v /html:/root/msdir/html -v /template:/root/msdir/template -v /upload:/root/msdir/upload --restart=always 192.168.0.45/dn_gate_dev/mcmsimg:1.00 &&\
+sudo docker run -d --name mcms -p 8080:8080 -v /root/msdir/upload:/upload --restart=always 192.168.0.45/dn_gate_dev/mcmsimg:1.00 &&\
 sudo docker logout &&\
 echo "部署成功,请根据接口文档访问测试项目"
